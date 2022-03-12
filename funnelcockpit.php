495eaf274dc4591263563b87ad00d0e060fcd585
gbhrdt <marius.gebhardt@gmail.com>
2022-03-12 18:18:16 +0100

    Release 1.3.5

--- funnelcockpit.php
+++ funnelcockpit.php
@@ -16,7 +16,7 @@
  * Plugin Name:       FunnelCockpit
  * Plugin URI:        https://funnelcockpit.com
  * Description:       Die All-In-One Lösung für den Aufbau von Funnels, Seiten und Conversion-Optimierung via Splittests, Maustracking und mehr.
- * Version:           1.3.4
+ * Version:           1.3.5
  * Author:            FunnelCockpit
  * Author URI:        https://funnelcockpit.com
  * License:           GPL-2.0+
--- public/class-funnelcockpit-public.php
+++ public/class-funnelcockpit-public.php
@@ -54,52 +54,6 @@ class FunnelCockpit_Public {
 
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in funnelcockpit_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The funnelcockpit_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		// wp_enqueue_style( $this->funnelcockpit, plugin_dir_url( __FILE__ ) . 'css/funnelcockpit-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in funnelcockpit_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The funnelcockpit_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		// wp_enqueue_script( $this->funnelcockpit, plugin_dir_url( __FILE__ ) . 'js/funnelcockpit-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
 	public function funnelpage_template($single) {
 		global $post;
 
@@ -113,7 +67,6 @@ class FunnelCockpit_Public {
 			remove_action( 'wp_head', 'rsd_link' );
 			remove_action( 'wp_head', 'wlwmanifest_link');
 			remove_theme_support( 'automatic-feed-links' );
-			add_theme_support( 'title-tag' );
 			add_filter('show_admin_bar', '__return_false');
 
 			// setup funnel page
